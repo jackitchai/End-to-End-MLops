70167b1a6d87b80e0b6d4c21913c4dc04e7070df
Kitchai Chaomuangchai <j_ckitchai@outlook.com>
2024-05-22 19:42:36 +0700

    validation

--- main.py
+++ main.py
@@ -11,4 +11,13 @@ except Exception as e:
         logger.exception(e)
         raise e
  
- 
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
